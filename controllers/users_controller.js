4f96593b0f1c5dbfa0fa48095be7b6ffdd98be00
Mayur <mayurnegi1999@gmail.com>
2020-07-06 09:07:31 +0530

    avatar action completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const path = require("path");
+const fs = require("fs");
 
 // let's keep it same as before
 module.exports.profile = function (req, res) {
@@ -34,6 +36,10 @@ module.exports.update = async function (req, res) {
         user.email = req.body.email;
 
         if (req.file) {
+          if (user.avatar) {
+            fs.unlinkSync(path.join(__dirname, "..", user.avatar));
+          }
+
           // this i ssaving the path of the uploaded file into the avatar field in the user
           user.avatar = User.avatarPath + "/" + req.file.filename;
         }
