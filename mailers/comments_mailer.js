106bf930d9d36e493f511fe8f96409b6e910b6c7
Mayur <mayurnegi1999@gmail.com>
2020-07-08 06:18:16 +0530

    rendered template

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -3,6 +3,11 @@ const { getMaxListeners } = require("../models/user");
 
 // this is another way of exporting method
 exports.newComment = (comment) => {
+  let htmlString = nodeMailer.renderTemplate(
+    { comment: comment },
+    "/comments/new_comment.ejs"
+  );
+
   console.log("inside newComment mailer", comment);
 
   nodeMailer.transporter.sendMail(
@@ -10,7 +15,7 @@ exports.newComment = (comment) => {
       from: "mayurnegi1999@gmail.com",
       to: "mayurnegi1999@gmail.com", // comment.user.email,
       subject: "New Comment Published",
-      html: "<h1>Your comment is now published</h1>",
+      html: htmlString,
     },
     (err, info) => {
       if (err) {
